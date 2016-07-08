d156226931dae50b91edbadf2d5fb2a2fa2620d1
junxu <junxu@anjuke.com>
2016-07-08 18:22:22 +0800

    调整屏幕适配，加入butter knife绑定控件

--- app/src/main/java/com/example/junxu/newPoly/MainActivity.java
+++ app/src/main/java/com/example/junxu/newPoly/MainActivity.java
@@ -1,81 +1,84 @@
 package com.example.junxu.newPoly;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.Volley;
-import com.android.volley.Response.*;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class MainActivity extends BasicActivity {
+
+    @BindView(R.id.bt_toutiao)
+    Button btToutiao;
+    @BindView(R.id.bt_tiyu)
+    Button btTiyu;
+    @BindView(R.id.bt_caijing)
+    Button btCaijing;
+    @BindView(R.id.list_item)
+    RecyclerView listItem;
+
     private RequestQueue mQueue;
-    private RecyclerView recyclerView;
-    private Button bt_toutiao;
-    private Button bt_tiyu;
-    private Button bt_caijing;
-    private MyListAdapter myListAdapter;
+    MyListAdapter myListAdapter;
     private static final String newTopURL = "http://v.juhe.cn/toutiao/index?key=f680bf9f08cf266d37a3736cbd5e1fcf&type="; //聚合数据
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bt_toutiao = (Button)findViewById(R.id.bt_toutiao);
-        bt_tiyu = (Button)findViewById(R.id.bt_tiyu);
-        bt_caijing = (Button)findViewById(R.id.bt_caijing);
-        bt_toutiao.setOnClickListener(this);
-        bt_tiyu.setOnClickListener(this);
-        bt_caijing.setOnClickListener(this);
+        ButterKnife.bind(this);
 
         initRecycleView();
         mQueue = Volley.newRequestQueue(this);
         getResultRespone("tiyu");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.new_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar snackbar = Snackbar.make(view, "暂时没有内容", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Action", null).show();
+            }
+        });
     }
 
-    private void initRecycleView(){
-        recyclerView = (RecyclerView) findViewById(R.id.list_item);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
+    private void initRecycleView() {
+        //recyclerView = (RecyclerView) findViewById(R.id.list_item);
+        listItem.setLayoutManager(new LinearLayoutManager(this));
+        listItem.setHasFixedSize(true);
         //recyclerView.setAdapter(new MyListAdapter(this,null));
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bt_toutiao:
-                getResultRespone("keji");
-                break;
 
-            case R.id.bt_tiyu:
-                getResultRespone("tiyu");
-                break;
-
-            case R.id.bt_caijing:
-                getResultRespone("caijing");
-                break;
-        }
-    }
-
-/**
- * 图片加载
- */
-    private void getImage(String imgURL){
+    /**
+     * 图片加载
+     */
+    private void getImage(String imgURL) {
         ImageRequest imageRequest = new ImageRequest(imgURL,
-                new Response.Listener<Bitmap>() {
+                new Listener<Bitmap>() {
                     @Override
                     public void onResponse(Bitmap bitmap) {
                         //imgView.setImageBitmap(bitmap);
                     }
                 }, 120, 120, Bitmap.Config.RGB_565,
-                new Response.ErrorListener() {
+                new ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError volleyError) {
                         volleyError.printStackTrace();
@@ -84,24 +87,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mQueue.add(imageRequest);
     }
 
-/**
- * Gson数据解析
- */
-    private void getResultRespone(String type){
-       MyJsonRequeset<ResultRespone> gsonRequest = new MyJsonRequeset<ResultRespone>(
-               newTopURL,type,ResultRespone.class,topnewInfoListenercallback(),topnewInfoErrorListenercallback()
-       );
+    /**
+     * Gson数据解析
+     */
+    private void getResultRespone(String type) {
+        MyJsonRequeset<ResultRespone> gsonRequest = new MyJsonRequeset<ResultRespone>(
+                newTopURL, type, ResultRespone.class, topnewInfoListenercallback(), topnewInfoErrorListenercallback()
+        );
         mQueue.add(gsonRequest);
     }
 
-    private Listener<ResultRespone> topnewInfoListenercallback(){
+    private Listener<ResultRespone> topnewInfoListenercallback() {
         Listener<ResultRespone> listener = new Listener<ResultRespone>() {
             @Override
             public void onResponse(ResultRespone result) {
-                if(result.getErr_code() == 0){  //数据返回成功
+                if (result.getErr_code() == 0) {  //数据返回成功
                     List<TopnewInfo> resultRespone = result.getResult().getData();//返回新闻列表
                     ShowNewList(resultRespone);
-                }else {
+                } else {
                     result.getReason(); //获取服务器返回错误类型
                 }
             }
@@ -109,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return listener;
     }
 
-    private ErrorListener topnewInfoErrorListenercallback(){
+    private ErrorListener topnewInfoErrorListenercallback() {
         ErrorListener errorListener = new ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
@@ -119,9 +122,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return errorListener;
     }
 
-    private void ShowNewList(final List<TopnewInfo> data){
-        myListAdapter = new MyListAdapter(this,data);
-        recyclerView.setAdapter(myListAdapter);
+    private void ShowNewList(final List<TopnewInfo> data) {
+        myListAdapter = new MyListAdapter(this, data);
+        listItem.setAdapter(myListAdapter);
         myListAdapter.setOnItemClickListener(new MyListAdapter.OnItemClickListener() {
             @Override
             public void OnItemClick(View view, int position) {
@@ -135,5 +138,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+    }
+
+
+    @OnClick({R.id.bt_toutiao, R.id.bt_tiyu, R.id.bt_caijing})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_toutiao:
+                getResultRespone("keji");
+                break;
+            case R.id.bt_tiyu:
+                getResultRespone("tiyu");
+                break;
+            case R.id.bt_caijing:
+                getResultRespone("caijing");
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/junxu/newPoly/MyListAdapter.java
+++ app/src/main/java/com/example/junxu/newPoly/MyListAdapter.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
@@ -17,25 +18,25 @@ import com.android.volley.toolbox.Volley;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by junxu on 2016/7/6.
  */
-public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.MyViewHolder> {
+public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder> {
     private Context context;
     private List<TopnewInfo> data;
     private RequestQueue mQueue;
     private ImageLoader mImageLoader;
-    private DisplayMetrics dm;
 
     public MyListAdapter(Context context, List<TopnewInfo> data) {
         this.context = context;
         this.data = data;
         initImageQueue();
-        Resources resources = context.getResources();
-        dm = resources.getDisplayMetrics();
     }
 
-    private void initImageQueue(){
+    private void initImageQueue() {
         mQueue = Volley.newRequestQueue(context);
         mImageLoader = new ImageLoader(mQueue, new ImageLoader.ImageCache() {
             @Override
@@ -55,20 +56,21 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.MyViewHold
         return data.size();
     }
 
-    public interface OnItemClickListener{
-        void OnItemClick(View view,int position);
-        void OnItemLongClick(View view,int position);
+    public interface OnItemClickListener {
+        void OnItemClick(View view, int position);
+
+        void OnItemLongClick(View view, int position);
     }
 
     private OnItemClickListener mOnItemClickListener;
 
-    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener){
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener) {
         this.mOnItemClickListener = mOnItemClickListener;
     }
 
     @Override
-    public void onBindViewHolder(final MyViewHolder myViewHolder, final int i) {
-        myViewHolder.tv_title.setText(data.get(i).getTitle());
+    public void onBindViewHolder(final ViewHolder myViewHolder, final int i) {
+        /*myViewHolder.tv_title.setText(data.get(i).getTitle());
         myViewHolder.tv_type.setText(data.get(i).getType());
         myViewHolder.tv_realtype.setText(data.get(i).getRealtype());
         myViewHolder.tv_author.setText(data.get(i).getAuthor_name());
@@ -76,32 +78,43 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.MyViewHold
         myViewHolder.iv_new.setDefaultImageResId(R.drawable.loading);  //设置加载时的默认显示图片
         myViewHolder.iv_new.setErrorImageResId(R.drawable.loading);  //设置加载失败显示的图片
         myViewHolder.iv_new.setImageUrl(data.get(i).getThumbnail_pic_s(), mImageLoader);  //设置URL
+        */
+        myViewHolder.newTitle.setText(data.get(i).getTitle());
+        myViewHolder.newType.setText(data.get(i).getType());
+        myViewHolder.newRealtype.setText(data.get(i).getRealtype());
+        myViewHolder.newAuthor.setText(data.get(i).getAuthor_name());
+        myViewHolder.newDate.setText(data.get(i).getDate());
+        myViewHolder.newImg.setDefaultImageResId(R.drawable.loading);  //设置加载时的默认显示图片
+        myViewHolder.newImg.setErrorImageResId(R.drawable.loading);  //设置加载失败显示的图片
+        myViewHolder.newImg.setImageUrl(data.get(i).getThumbnail_pic_s(), mImageLoader);  //设置URL
 
         //myViewHolder.itemView.setTag(context);
         myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mOnItemClickListener.OnItemClick(myViewHolder.itemView,i);
+                mOnItemClickListener.OnItemClick(myViewHolder.itemView, i);
             }
         });
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View itemView = LayoutInflater.from(context).
-                inflate(R.layout.cell_topnew,viewGroup,false);
-        MyViewHolder myViewHolder = new MyViewHolder(itemView);
+                inflate(R.layout.cell_topnew, viewGroup, false);
+        ViewHolder myViewHolder = new ViewHolder(itemView);
         return myViewHolder;
     }
-
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+/*
+    public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView tv_title;
         TextView tv_type;
         TextView tv_realtype;
         TextView tv_author;
         TextView tv_date;
         NetworkImageView iv_new;
+
         LinearLayout ll;
+
         public MyViewHolder(View itemView) {
             super(itemView);
             tv_title = (TextView) itemView.findViewById(R.id.new_title);
@@ -112,13 +125,33 @@ public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.MyViewHold
             tv_date = (TextView) itemView.findViewById(R.id.new_date);
             iv_new = (NetworkImageView) itemView.findViewById(R.id.new_img);
             iv_new.setAdjustViewBounds(true);
-            iv_new.setMaxWidth(dm.widthPixels/5);
-            iv_new.setMaxHeight(dm.heightPixels/10);
+            iv_new.setMaxWidth(dm.widthPixels / 5);
+            iv_new.setMaxHeight(dm.heightPixels / 10);
 
             ll = (LinearLayout) itemView.findViewById(R.id.new_contextlay);
-            ll.setLayoutParams(new LinearLayout.LayoutParams(4*dm.widthPixels/5-25,dm.heightPixels/10));
+            //ll.setLayoutParams(new LinearLayout.LayoutParams(4 * dm.widthPixels / 5 - 30, dm.heightPixels / 10));
         }
     }
+*/
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        @BindView(R.id.new_img)
+        NetworkImageView newImg;
+        @BindView(R.id.new_type)
+        TextView newType;
+        @BindView(R.id.new_realtype)
+        TextView newRealtype;
+        @BindView(R.id.new_author)
+        TextView newAuthor;
+        @BindView(R.id.new_title)
+        TextView newTitle;
+        @BindView(R.id.new_contextlay)
+        LinearLayout newContextlay;
+        @BindView(R.id.new_date)
+        TextView newDate;
 
-
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
 }
